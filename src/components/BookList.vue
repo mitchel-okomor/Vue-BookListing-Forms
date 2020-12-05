45e0cd9c62165cccfe5a554225fe982f080e9c00
CodeXaver <mitchel.okomor@gmail.com>
2020-12-05 17:18:14 +0100

    added filter

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,12 +4,21 @@
     <ul>
       <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
     </ul>
+    <hr>
+    <h2>Filtered Books By Ownership</h2>
+    <select  v-model="holding">
+      <option v-for="filter in filters" :key="filter.id">{{filter}}</option>
+    </select>
+     <ul>
+      <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
+    </ul>
     <br><hr>
     <book-form @addBook='appendBook'></book-form>
   </div>
 </template>
 
 <script>
+import _ from "lodash";
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
 
@@ -17,14 +26,21 @@ export default {
   name: "BookList",
   data() {
     return {
+      holding: "bought",
+      filters: ["bought", "borrowed"],
       title: "All Books",
       states: ["Want to Read", "Read", "Reading"],
       books: [
-        { title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading:true },
-        { title: "American Gods", author: "Neil Gaiman", finishedReading: false },
-        { title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true }
+        { title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading:true, ownership: "borrowed" },
+        { title: "American Gods", author: "Neil Gaiman", finishedReading: false, ownership: "bought" },
+        { title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true, ownership: "borrowed" }
       ]
     };
+  },
+  computed:{
+    filteredBooks(){
+        return _.filter(this.books, ["ownership", this.holding]);
+      }
   },
   components: {
     BookItem,
@@ -32,7 +48,7 @@ export default {
   },
   methods: {
     appendBook(bookData) {
-      this.books.push({ title: bookData.bookTitle, author: bookData.bookAuthor, finishedReading:bookData.finishedReading });
+      this.books.push({ title: bookData.bookTitle, author: bookData.bookAuthor, finishedReading:bookData.finishedReading, ownership: bookData.ownership });
     }
   }
 };
